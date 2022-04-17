8cc1d019dd0e73c22c2293b74ce53a5c697d281f
Hector Santana <[hector.santana.gc@gmail.com]>
2022-04-17 23:15:22 +0100

    subi el ejercicio final del curso practico de javascript

--- README.md
+++ README.md
@@ -30,3 +30,10 @@
 - Segundo paso: implementar las fórmulas en JavaScript
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JS con HTML
+
+## Taller #5: Ejercicio práctico - Calculadora ahorro e interés compuesto
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
---
+++ ahorro.js
@@ -0,0 +1,53 @@
+// Calculadora de ahorro mensual según porcentaje elegido
+
+function calcularAhorroMensual(ingresos, porcentaje) {
+    const ahorro = (ingresos * porcentaje) / 100;
+    return ahorro;
+}
+
+function ButtonCalcularAhorroMensual() {
+    const inputIngresos = document.getElementById("InputIngresos");
+    const ingresosValue = inputIngresos.value;
+
+    const inputPorcentaje = document.getElementById("InputAhorro");
+    const ahorroValue = inputPorcentaje.value;
+
+    const ahorroMensual = calcularAhorroMensual(ingresosValue, ahorroValue);
+
+    const ResultA = document.getElementById("ResultA");
+    ResultA.innerText = "El " + ahorroValue + "% de tus ingresos es " + ahorroMensual + "€";
+}
+
+
+// Calculadora de interés compuesto
+
+function calcularInteresCompuesto(ahorros, tiempo, interes) {
+    let interesCompuesto = 0;
+    let ahorroTotal = 0;
+
+    for (i = 0; i < tiempo ; i++) {
+        ahorroTotal = interesCompuesto + ahorros;
+        interesCompuesto = ahorroTotal * (1 + interes / 100);
+    }
+
+    return interesCompuesto.toFixed(2);
+}
+
+function ButtonCalcularInteresCompuesto() {
+    const inputAhorroMensual = document.getElementById("InputAhorroMensual");
+    const ahorroMensualValue = inputAhorroMensual.value;
+
+    const aportacionAnual = ahorroMensualValue * 12;
+
+    const inputTiempo = document.getElementById("InputTiempo");
+    const tiempoValue = inputTiempo.value;
+
+    const inputInteres = document.getElementById("InputInteres");
+    const interesValue = inputInteres.value;
+    
+    const resultadoFinal = calcularInteresCompuesto(aportacionAnual, tiempoValue, interesValue);
+
+    const ResultB = document.getElementById("ResultB");
+    ResultB.innerText = "El capital final invirtiendo " + ahorroMensualValue + "€ al mes con un interés del " + interesValue + "% durante " + 
+    tiempoValue + " años sería " + resultadoFinal + "€";
+}
